6b39a43c94e53af9dbab3a6f2690ccd4aeb9b2a6
sheikhlimon <sheikhlimon404@gmail.com>
2021-03-23 10:04:09 +0600

    dynamic api making

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send("Thanks for sending me"));
+const users = ["Raisa", "Nasir", "Limon", "Tuhin", "Elina", "Oni"]
+
+const fruit = {
+    product: "ada",
+    price : 10
+}
+
+app.get("/fruits/banana", (req, res) => {res.send({product:" banana", quantity: 1000, price: 10000})})
+
+app.get('/', (req, res) => res.send(fruit));
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+})
 
 app.listen(3000, () => console.log('Listening to port 3000'));
